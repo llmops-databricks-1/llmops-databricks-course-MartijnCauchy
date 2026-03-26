402dbd69ec3ca04fb68f322177f126ca5e0f63ef
Martijn <martijn@cauchy.io>
2026-03-26 10:55:27 +0100

    add budget policy

--- notebooks/L1_0_catalog_and_schema.py
+++ notebooks/L1_0_catalog_and_schema.py
@@ -1,4 +1,7 @@
 # Databricks notebook source
+# Notebook to create catalog and schema for the course.
+# You can run this notebook multiple times without error, 
+# as it uses "IF NOT EXISTS" in the SQL commands.
 
 from loguru import logger
 from pyspark.sql import SparkSession
@@ -8,7 +11,7 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.getOrCreate()
 
 # Schema name 
-CATALOG = "llmops_dev"
+CATALOG = "llmops_dev_martijn"
 SCHEMA = "arxiv"  # Change this to your desired schema name
 
 # Create catalog if it doesn't exist
@@ -18,4 +21,5 @@ logger.info(f"Catalog {CATALOG} ready")
 # Create schema if it doesn't exist
 spark.sql(f"CREATE SCHEMA IF NOT EXISTS {CATALOG}.{SCHEMA}")
 logger.info(f"Schema {CATALOG}.{SCHEMA} ready")
+
 # COMMAND ----------
